d36361385be5173e4c8c3f9546810ef477ddf274
Angel D Hit <phamdangphat1@gmail.com>
2025-04-08 14:38:35 +0700

    add manage page

--- MusicWebMVC/Controllers/AdminController.cs
+++ MusicWebMVC/Controllers/AdminController.cs
@@ -1,16 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MusicWeb.Models;
+using MusicWebMVC.Data;
 
 namespace MusicWebMVC.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult NewFeedAdmin()
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Dashboard()
         {
             return View();
         }
-        public IActionResult Manage()
+
+        //public IActionResult User()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Post()
         {
             return View();
+        }
+
+        public IActionResult Cover()
+        {
+            return View();
+        }
+
+        public IActionResult User()
+        {
+            var users = _context.Users
+                .Where(u => u.Role == "User")
+                .Select(u => new UserViewModel
+                {
+                    Id = u.Id,
+                    Username = u.Username,
+                    Email = u.Email,
+                    PostCount = u.Posts.Count()
+                })
+                .ToList();
+
+            return View(users);
         }
     }
 }
