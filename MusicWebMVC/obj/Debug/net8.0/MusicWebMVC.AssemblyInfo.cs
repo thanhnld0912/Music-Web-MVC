05fab12ce08a3d68f490ddffdd8673b54bd5a477
Angel D Hit <phamdangphat1@gmail.com>
2025-04-22 16:46:50 +0700

    checkout main

--- MusicWebMVC/obj/Debug/net8.0/MusicWebMVC.AssemblyInfo.cs
+++ MusicWebMVC/obj/Debug/net8.0/MusicWebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1353cfa5c3769c16b18471cdd864627e70d3939")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103f0607c8218a49986205c4fefc762a8a598b80")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
