bfc2746b39695847305896e0b85c0abf36b0669e
Angel D Hit <phamdangphat1@gmail.com>
2025-04-23 15:02:54 +0700

    commit before merge

--- MusicWebMVC/obj/Debug/net8.0/MusicWebMVC.AssemblyInfo.cs
+++ MusicWebMVC/obj/Debug/net8.0/MusicWebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4172ca019814785f76e491e495b32819f56974")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f81956e4d8a07d45f5532570758b630ac4d5eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
